900bffff9fac4d793e383a67a486003c7091efdf
tunzor <russo555@gmail.com>
2019-07-29 21:20:58 -0400

    Added date and timestamp to show dynamic content from python in the HTML document.

--- ex4-flask-web-server.py
+++ ex4-flask-web-server.py
@@ -4,6 +4,8 @@
 # Import the flask library; this gives us a web server which
 # we can connect to and view our website/web application
 from flask import Flask
+# This allows us to access date and time functions
+import datetime
 
 # Setup the app variable for flask
 app = Flask(__name__)
@@ -21,13 +23,24 @@ def index():
         </head>
         <body style="background-color: LightSkyBlue;">
             <h1>Hello Ladies Learning Code!</h1>
+            <h3>Today is <span style="color: Firebrick;">{}</span> 
+                and the current time is <span style="color: Firebrick;">{}</span></h3>
             <p>Welcome to Flask running on Python!</p>
             <p>You can learn more about flask by clicking 
                 <a href="https://en.wikipedia.org/wiki/Flask_(web_framework)">here</a>
             </p>
         </body>
         </html>
-    '''
+    '''.format(datetime.datetime.now().strftime('%B %d, %Y'), datetime.datetime.now().strftime('%H:%M:%S'))
+# datetime.datetime.now().strftime('%B %d %Y') gets the current date/time
+# and formats it as MONTH DAY, YEAR (eg. July 25, 2019);
+# datetime.datetime.now().strftime('%H:%M:%S') formats the time as HH:MM:SS (eg.12:01:11)
+
+# The format function used above takes the formatted date and substitutes it
+# in for the first occurrence of the curly braces {} on this line:
+# <h3>Today is <span style="color: Firebrick;">{}</span>
+# and does the same for the second part for the time as 12:01:22 here:
+# the current time is <span style="color: Firebrick;">{}</span></h3>
 
 
 # Flask runs it's server on localhost with a default
